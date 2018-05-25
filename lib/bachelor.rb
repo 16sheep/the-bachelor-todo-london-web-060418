4026eab48491ae62b3ce40e050d659c4ad797313
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-25 13:51:03 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,5 +52,7 @@ def get_average_age_for_season(data, season)
   data[season].each { |data|
     age = age + data["age"].to_f.ceil
   }
+  print age
+  print data[season].length
   age / data[season].length
 end
