2334f53d938f4ddfa6ce29fea65caac1b156922d
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-25 13:36:55 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -36,7 +36,15 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+   data.each {|current_season, season_data|
+   #binding.pry
+    season_data.each { |current_data|
+    #binding.pry
+      if current_data["hometown"] == hometown
+        return current_data["occupation"]
+      end
+    }  
+  }
 end
 
 def get_average_age_for_season(data, season)
