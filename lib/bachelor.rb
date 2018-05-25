7784f373eca905c23c4cca667f8166379c292074
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-25 13:30:20 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,25 @@
+require 'pry'
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each { |data|
+    if data["status"] == "Winner"
+      first_name = data["name"].split(" ")
+      return first_name[0]
+    end
+  }
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each {|current_season, season_data|
+   binding.pry
+    season_data.each { |current_data|
+    binding.pry
+      if current_data["occupation"] == occupation
+        first_name = data["name"]
+        print first_name
+      end
+    }  
+  }
+  return "lol"
 end
 
 def count_contestants_by_hometown(data, hometown)
